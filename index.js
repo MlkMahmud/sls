dec374d2e808ccbdf3dce8f06b5e232a1b6f2e6b
Malik Mahmud <almalikmahmud@gmail.com>
2022-07-08 12:48:18 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const serverless = require('serverless-http');
+const { createApp } = require('@lhci/server');
+
+let handler;
+
+module.exports.handler = async (event, context, callback) => {
+  try {
+    if (!handler) {
+      const { app } = await createApp({
+        storage: {
+          storageMethod: 'sql',
+          sqlDialect: 'postgres',
+          sqlConnectionSsl: true,
+          sqlConnectionUrl: process.enb.DB_URL,
+          sqlDialectOptions: { ssl: true },
+        },
+      });
+      handler = serverless(app);
+    }
+    const result = await handler(event, context);
+    return result;
+  } catch (err) {
+    console.error(err);
+    callback(err);
+  }
+};
